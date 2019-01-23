eacfda35a069958279b857d8351f76b77025d21a
liuwei <850176300@qq.com>
2019-01-23 17:02:59 +0800

    no message

--- routes/all.js
+++ routes/all.js
@@ -40,7 +40,19 @@ router.post('/login', function(req, res, next) {
         }
     }
     var req = require(isHttp?'http':'https').request(options, function(res){
-        console.log("--------------res-------------", res)
+        console.log(res.httpVersion);
+        console.log(res.headers);
+
+        console.log(res.trainers);
+
+        console.log(res.method);
+
+        console.log(res.url);
+
+        console.log(res.statusCode);
+
+        console.log(res.socket);
+
     })
 
     req.write(content)
